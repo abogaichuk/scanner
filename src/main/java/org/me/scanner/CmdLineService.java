7e8905d20003e7f0119fdb6653ef4c296110bedf
Andrii Bohaichuk <bogaychuk.a@gmail.com>
2021-09-08 14:11:26 +0300

    refactoring

--- src/main/java/org/me/scanner/CmdLineService.java
+++ src/main/java/org/me/scanner/CmdLineService.java
@@ -2,13 +2,14 @@ package org.me.scanner;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.PrintWriter;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.joining;
 import static org.me.scanner.ScannerUtils.getFirstArgument;
 import static org.me.scanner.ScannerUtils.isNotEmpty;
 
@@ -18,21 +19,8 @@ public class CmdLineService {
     public String buildCommand(CommandLine cmd) {
         final String host = getFirstArgument(cmd);
         return Stream.of(cmd.getOptions())
-                .map(option -> {
-                    StringBuilder sb = new StringBuilder();
-                    if (isNotEmpty(option.getOpt())) {
-                        sb.append("-");
-                        sb.append(option.getOpt());
-                        if (option.hasArg() && isNotEmpty(option.getValue())) {
-                            sb.append(" ");
-                            sb.append(option.getValue());
-                        }
-                    } else {
-                        sb.append("--");
-                        sb.append(option.getLongOpt());
-                    }
-                    return sb.toString();
-                }).collect(Collectors.joining(" ", "nmap ", host));
+                .map(this::handle)
+                .collect(joining(" ", "nmap ", host));
     }
 
     public void printHelp(Options options) {
@@ -50,5 +38,21 @@ public class CmdLineService {
                 "--- HELP ---",
                 false);
         writer.flush();
+    }
+
+    private String handle(Option option) {
+        StringBuilder sb = new StringBuilder();
+        if (isNotEmpty(option.getOpt())) {
+            sb.append("-");
+            sb.append(option.getOpt());
+            if (option.hasArg() && isNotEmpty(option.getValue())) {
+                sb.append(" ");
+                sb.append(option.getValue());
+            }
+        } else {
+            sb.append("--");
+            sb.append(option.getLongOpt());
+        }
+        return sb.toString();
     }
 }
